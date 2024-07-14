64c0470bec1ece2d750943e8f4412c9afd2b3afc
Monalisa Onyango <+>
2024-07-14 13:42:22 +0300

    using if

--- index.js
+++ index.js
@@ -29,6 +29,19 @@
 
 
  let superbowlWin = (record) => {
+
     const result = record.find( record => record.result === "W" );
-    return !!result ? result.year : undefined;
+
+    // return !!result ? result.year : undefined;
+
+
+    if (result){
+      return result.year
+     
+    }else {
+        return undefined
+      }
   }
+  
+
+  
